04da035dac03828f0cd01e0d6a1f72901568fdda
VinSalus <viniciusmsalustiano@hotmail.com>
2022-02-02 14:01:58 -0300

    Completei mais aulas.

--- 2. Practice time - part 1/2. Contatenate two strings in a function/index.js
+++ 2. Practice time - part 1/2. Contatenate two strings in a function/index.js
@@ -1,5 +1,11 @@
-let name = "Linda"
+let aName = "Linda"
 let greeting = "Hi there"
 
 // Create a function that logs out "Hi there, Linda!" when called
 
+function logGreeting(){
+    let fullGreeting = greeting + ", " + aName + "!";
+    console.log(fullGreeting);
+}
+
+logGreeting();
--- 3. Build a Blackjack game/3. If...else conditionals/index.js
+++ 3. Build a Blackjack game/3. If...else conditionals/index.js
@@ -1,4 +1,11 @@
-let firstCard = 6
-let secondCard = 9
-let sum = firstCard + secondCard
+let firstCard = 6;
+let secondCard = 16;
+let sum = firstCard + secondCard;
 
+if(sum < 21){
+    console.log("Do you want to draw another card? 😐")
+} else if(sum === 21 ){
+    console.log("Blackjack! You've won! 🥳")
+} else{
+    console.log("You're out of the game! 😭")
+}
