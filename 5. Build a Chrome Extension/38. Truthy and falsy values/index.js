eba1c5fa4c8e0b40b5993a5a4f1c70f55b7f8628
VinSalus <viniciusmsalustiano@hotmail.com>
2022-02-10 12:39:42 -0300

    Completei mais aulas

--- 5. Build a Chrome Extension/38. Truthy and falsy values/index.js
+++ 5. Build a Chrome Extension/38. Truthy and falsy values/index.js
@@ -1,6 +1,6 @@
-console.log(  Boolean("")   ) //
-console.log(  Boolean("0")  ) //
-console.log(  Boolean(100)  ) //
-console.log(  Boolean(null) ) //
-console.log(  Boolean([0])  ) //
-console.log(  Boolean(-0)   ) //
+console.log(  Boolean("")   ) // F
+console.log(  Boolean("0")  ) // T
+console.log(  Boolean(100)  ) // T
+console.log(  Boolean(null) ) // F
+console.log(  Boolean([0])  ) // T because 0 is falsy but the array is truthy.
+console.log(  Boolean(-0)   ) // F because 0 is falsy. Negative numbers aren't falsys tho.
